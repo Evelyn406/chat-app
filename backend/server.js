cd34f1e65a695e28fd0a386a866fd91c8bcc8a5d
Ruijie Xia <ruijie@g.ucla.edu>
2024-04-16 22:57:23 +0800

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
+import path from 'path';
 
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
@@ -10,6 +11,9 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
+
 dotenv.config();
  
 app.use(express.json()); // to parse the incoming requests with JSON from req.body
@@ -19,6 +23,12 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //     // root route http://localhost:3000/
